41b13f21b38cbc591ccab1b4697011d129bd6537
AntoninDev <118391001+AntoninDev@users.noreply.github.com>
2022-12-20 10:52:16 -0300

    Update horadodia.js

--- pages/script/horadodia.js
+++ pages/script/horadodia.js
@@ -14,20 +14,21 @@ const escreve = document.getElementById("horaDia")
     body.style.background = '#020101'
     body.style.color = 'white'
     escreve.innerHTML = '<h1>Hora da Madrugada</h1>'
-    section.innerHTML= `<p class="msg">Boa madrugada, agora são ${horapm}:${min} e ${sec} segundos.</p><br><img src="midia/images/madrugada.png" class="imgmadrugada">`
+    section.innerHTML= `<p>Boa madrugada, agora são ${horapm}:${min} e ${sec} segundos.</p><br><img src="midia/images/madrugada.png" class="imgmadrugada">`
   }else if (hora < 12) {
     body.style.background = '#FAF86A'
     escreve.innerHTML = '<h1>Hora do Dia</h1>'
-    section.innerHTML= `<p>Bom dia, agora são ${horapm}:${min} e ${sec} segundos.</p><br> <img src="midia/images/dia.png"><p>IMAGEM BUGADA ARRUMO AO ACORDAR MO SONO</p>`
+    section.innerHTML= `<p>Bom dia, agora são ${horapm}:${min} e ${sec} segundos.</p><br> <img class="imgdia" src="midia/images/dia.png">`
     }else if (hora < 18) {
     body.style.background = '#759FF4'
     escreve.innerHTML = '<h1>Hora da Tarde</h1>'
-    section.innerHTML= `<p class="msg">Boa tarde, agora são ${horapm}:${min} e ${sec} segundos.</p><br><img src="midia/images/tarde.png" class="imgtarde">`
+    section.innerHTML= `<p>Boa tarde, agora são ${horapm}:${min} e ${sec} segundos.</p><br><img src="midia/images/tarde.png" class="imgtarde">`
   }else if (hora <= 23) {
     body.style.background = '#1F1F20'
     body.style.color = 'white'
     escreve.innerHTML = '<h1>Hora da Noite</h1>'
-    section.innerHTML= `<p class="msg">Boa noite, agora são ${horapm}:${min} e ${sec} segundos.</p><br><img src="midia/images/noite.png" class="imgnoite">`
+    section.innerHTML= `<p>Boa noite, agora são ${horapm}:${min} e ${sec} segundos.</p><br><img src="midia/images/noite.png" class="imgnoite">`
   }else {
     escreve.innerHTML = '[ERROR] Hora invalida!'
   }  }, 1 );
+  

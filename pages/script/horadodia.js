f19ffecc8bdea13fc246a72695e143e7bd9ed5c2
AntoninDev <118391001+AntoninDev@users.noreply.github.com>
2022-12-20 00:40:14 -0300

    Add files via upload

--- pages/script/horadodia.js
+++ pages/script/horadodia.js
@@ -1,1 +1,33 @@
-
+const escreve = document.getElementById("horaDia")
+  const section = document.getElementById("escrita")
+  const body = document.getElementById("body")
+  setInterval( function () {
+  const hora = 6 //new Date().getHours()
+  if (hora >= 0 && hora <= 9) {
+    var horapm = `0${hora}`
+  }else {
+    horapm = hora
+  }
+  const min = new Date().getMinutes()
+  const sec = new Date().getSeconds()
+  if (hora >= 0 && hora < 6) {
+    body.style.background = '#020101'
+    body.style.color = 'white'
+    escreve.innerHTML = '<h1>Hora da Madrugada</h1>'
+    section.innerHTML= `<p class="msg">Boa madrugada, agora são ${horapm}:${min} e ${sec} segundos.</p><br><img src="midia/images/madrugada.png" class="imgmadrugada">`
+  }else if (hora < 12) {
+    body.style.background = '#FAF86A'
+    escreve.innerHTML = '<h1>Hora do Dia</h1>'
+    section.innerHTML= `<p>Bom dia, agora são ${horapm}:${min} e ${sec} segundos.</p><br> <img src="midia/images/dia.png">`
+    }else if (hora < 18) {
+    body.style.background = '#759FF4'
+    escreve.innerHTML = '<h1>Hora da Tarde</h1>'
+    section.innerHTML= `<p class="msg">Boa tarde, agora são ${horapm}:${min} e ${sec} segundos.</p><br><img src="midia/images/tarde.png" class="imgtarde">`
+  }else if (hora <= 23) {
+    body.style.background = '#1F1F20'
+    body.style.color = 'white'
+    escreve.innerHTML = '<h1>Hora da Noite</h1>'
+    section.innerHTML= `<p class="msg">Boa noite, agora são ${horapm}:${min} e ${sec} segundos.</p><br><img src="midia/images/noite.png" class="imgnoite">`
+  }else {
+    escreve.innerHTML = '[ERROR] Hora invalida!'
+  }  }, 1 );

e149a989198d017a4fd9879c38784cc36cc6fa19
AntoninDev <118391001+AntoninDev@users.noreply.github.com>
2022-12-26 01:18:41 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,19 +1,73 @@
+////////////////////////////////////////////////////////
+
   const paginas = document.getElementById("paginas")
   const descricao = document.getElementById("descricao")
-  const btnvolta = document.getElementById("btnvolta")
-  const btnpassa = document.getElementById("btnpassa")
+  const buttons = document.getElementById("buttons")
+  let a = document.createElement('a')
+  let img = document.createElement('img')
+  let p = document.createElement('p')
+  let p2 = document.createElement('p')
+  let h1 = document.createElement('h1')
+  let button = document.createElement('input')
+  let button2 = document.createElement('input')
+  
+  button.setAttribute('type', 'button')
+  button2.setAttribute('type', 'button')
+  button.setAttribute('class', 'left')
+  button.setAttribute('value', '<')
+  button2.setAttribute('value', '>')
+  buttons.appendChild(button)
+  buttons.appendChild(button2)
+     paginas.appendChild(a)
+     paginas.appendChild(p)
+     descricao.appendChild(h1)
+     descricao.appendChild(p2)
+     a.appendChild(img)
+       p.setAttribute('class', 'bolinhas')
+       p2.setAttribute('class', 'descricao')
+        h1.innerHTML = 'Descrição:'
+     
+     //Iicializa
+      a.setAttribute('href', 'pages/horadodia.html')
+      img.setAttribute('src', 'pages/midia/images/sitehorasdodia.jpg')
+      img.setAttribute('alt', 'Site Horas do dia')
+      p.innerHTML = '○●'
+      
+      //Descrição
+      p2.innerHTML = 'Horas do dia é um site simples que a cada hora do dia tem uma decoração diferente, Entre e veja clicando na imagem acima.'
+      
+      //Botão
+      button.setAttribute('onclick', 'passa()')
+      button2.setAttribute('onclick', 'passa()')
+      
     function passa() {
-      paginas.innerHTML = '<a href="pages/notadoaluno.html"> <img alt="Site Nota do aluno" src="pages/midia/images/sitenotadoaluno.jpg"></a><p class="bolinhas">●○</p>'
-      descricao.innerHTML = `<h1>Descrição:</h1>
-        <p class="descricao"><strong>Calculador de nota do aluno é uma calculadora simples que calcula se o aluno passou ou nao de acordo com suas notas obs: a ultima nota dobra, Entre e veja clicando na imagem acima.</strong></p>`
-        btnvolta.innerHTML = `<button class="left" onclick="volta()"><</button>`
-        btnpassa.innerHTML = `<button onclick="volta()">></button>`
+      a.setAttribute('href', 'pages/notadoaluno.html')
+      img.setAttribute('src', 'pages/midia/images/sitenotadoaluno.jpg')
+      img.setAttribute('alt', 'Site Nota do aluno')
+      p.innerHTML = '●○'
+      
+      //Descrição
+      p2.innerHTML = 'Calculador de nota do aluno é uma calculadora simples que calcula se o aluno passou ou nao de acordo com suas notas obs: a ultima nota dobra, Entre e veja clicando na imagem acima.'
+      
+      //Botão
+        button.setAttribute('onclick', 'volta()')
+        button2.setAttribute('onclick', 'volta()')
+        
         window.scroll({top: 1000, behavior:"smooth"})
+        
     }
     function volta() {
-      paginas.innerHTML = '<a href="pages/horadodia.html"> <img alt="Site Horas do dia" src="pages/midia/images/sitehorasdodia.jpg"></a><p class="bolinhas">○●</p>'
-      descricao.innerHTML = `<h1>Descrição:</h1>
-        <p class="descricao"><strong>Horas do dia é um site simples que a cada hora do dia tem uma decoração diferente, Entre e veja clicando na imagem acima.</strong></p>`
-        btnvolta.innerHTML = `<button class="left" onclick="passa()"><</button>`
-        btnpassa.innerHTML = `<button onclick="passa()">></button>`
+      
+      a.setAttribute('href', 'pages/horadodia.html')
+      img.setAttribute('src', 'pages/midia/images/sitehorasdodia.jpg')
+      img.setAttribute('alt', 'Site Horas do dia')
+      p.innerHTML = '○●'
+      //Descrição
+      
+      p2.innerHTML = 'Horas do dia é um site simples que a cada hora do dia tem uma decoração diferente, Entre e veja clicando na imagem acima.'
+      //Botão
+      
+      button.setAttribute('onclick', 'passa()')
+      button2.setAttribute('onclick', 'passa()')  
     }
+    ////////////////////////////////////////////////////

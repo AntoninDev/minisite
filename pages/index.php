410e7efa36425b23db5a8f0cae7f9db5e9059d50
AntoninDev <118391001+AntoninDev@users.noreply.github.com>
2022-12-26 22:29:58 -0300

    Add files via upload

---
+++ pages/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Cfgs padrão--> 
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Cor da barra de pesquisa --> 
+      <meta name="theme-color" content="#000000" />
+      
+    <!-- Favicon icone do site que aparece na aba --> 
+    <link rel="shortcut icon" href="#" />
+    
+      <!-- Link da pasta css --> 
+      <link rel="stylesheet" href="#" />
+      
+  <!-- Link  da pasta script--> 
+  <script src="#"></script>
+  
+    <title>TITULO NÃO DEFINIDO</title>
+    
+    </head>
+    <style>
+      body {
+        background: #a7a7a7c2;
+      }
+      section {
+        color: #0cd613c4;
+        background: black;
+        width: 75vw;
+        margin-top: 22vh;
+        margin-left: 10vw;
+        border: 0.5vw grey solid;
+        border-radius: 2vw;
+        font-size: 3vh;
+        padding: 2vw;
+      }
+    </style>
+    <body>
+      <section>
+        
+<?php
+/*PHP foi criado em 1994 por Rasmus Lerdorf
+PHP antes significa Personal Home Page
+Porém em 1998 seu nome foi alterado para PHP: Hypertext Preprocessor
+Atualmente PHP se enconta na versão 8.0
+client-side: Funciona do lado do 
+server-side: funciona do lado do servidor
+Typecast
+inteiro (int), (integer)
+real (real), (float), (double)
+caractere (string)
+             -> true = 1
+logico (???) -
+             -> false = vazio
+sendo ultilizado assim
+$ex = (string) 0;
+echo $ex
+mostraria "0" em forma de string
+concatenaçao é feita com .
+ex:
+$nome = "antonio"
+$idade = 15
+echo $nome." Tem ". $idade. " anos!";
+retona: "antonio Tem 15 Anos!"
+de forma mais pratica podemos usar
+echo "$nome Tem $idade anos!" que retorna o mesmo valor acima
+*/
+
+//para declarar uma variável 
+$nome = "antonio";
+$antonio = "nada";
+//para mostrar uma variavel
+echo $nome;
+$n1 = 3;
+$n2 = 2;
+$s = $n1 + $n2;
+echo "<br>".($n1 + $n2);
+echo "<br>".($n1 * $n2)
+?>
+      </section>
+      </body>
+</html> 
